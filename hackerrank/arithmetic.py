65dae49afab3dd46255f31c478aad1c4b67d460c
Irene-Busah <i.busah@alumni.alueducation.com>
2026-01-25 00:21:25 +0200

    update: added solution for division & arithmetic problems

---
+++ hackerrank/arithmetic.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    num_sum = a + b
+    print(num_sum)
+    diff = a - b
+    print(diff)
+    product = a * b
+    print(product)
