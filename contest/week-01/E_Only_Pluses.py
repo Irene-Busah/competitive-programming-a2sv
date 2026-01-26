7e64460aa7caa4c00d0247fe3df446520cfc8826
Irene-Busah <i.busah@alumni.alueducation.com>
2026-01-26 21:09:53 +0200

    update: completed solution for determine weird/not-weird values

--- contest/week-01/E_Only_Pluses.py
+++ contest/week-01/E_Only_Pluses.py
@@ -1,41 +1,38 @@
 """
-Kmes has written three integers 𝑎
-, 𝑏
- and 𝑐
- in order to remember that he has to give Noobish_Monk 𝑎×𝑏×𝑐
- bananas.
+Kmes has written three integers a, b and c in order to remember that he has to give Noobish_Monk axbxb
+bananas.
 
-Noobish_Monk has found these integers and decided to do the following at most 5
- times:
+Noobish_Monk has found these integers and decided to do the following at most 5 times:
 
-pick one of these integers;
-increase it by 1
-.
-For example, if 𝑎=2
-, 𝑏=3
- and 𝑐=4
-, then one can increase 𝑎
- three times by one and increase 𝑏
- two times. After that 𝑎=5
-, 𝑏=5
-, 𝑐=4
-. Then the total number of bananas will be 5×5×4=100
-.
+- pick one of these integers;
+- increase it by 1
 
-What is the maximum value of 𝑎×𝑏×𝑐
- Noobish_Monk can achieve with these operations?
+For example, if a=2, b=3 and c=4, then one can increase a three times by one and increase b
+two times. After that a=5, b=5, c=4. Then the total number of bananas will be 5x5x5=100
+
+What is the maximum value of axbxc Noobish_Monk can achieve with these operations?
 
 Input
-Each test contains multiple test cases. The first line of input contains a single integer 𝑡
- (1≤𝑡≤1000
-) — the number of test cases. The description of the test cases follows.
+======
+Each test contains multiple test cases. The first line of input contains a single integer t (1≤t≤1000) — the number of test cases. The description of the test cases follows.
 
-The first and only line of each test case contains three integers 𝑎
-, 𝑏
- and 𝑐
- (1≤𝑎,𝑏,𝑐≤10
-) — Kmes's integers.
+The first and only line of each test case contains three integers a, b and c (1≤a,b,c≤10) — Kmes's integers.
 
 Output
+=======
 For each test case, output a single integer — the maximum amount of bananas Noobish_Monk can get.
 """
+
+
+import sys
+
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+
+
--- hackerrank/weird.py
+++ hackerrank/weird.py
@@ -1,5 +1,6 @@
 """
 Task
+=====
 Given an integer, n, perform the following conditional actions:
 
 If n is odd, print Weird
@@ -19,3 +20,19 @@ Output Format
 ================
 Print Weird if the number is weird. Otherwise, print Not Weird.
 """
+
+
+
+def determine_weird(n: int):
+    
+    if (n % 2 != 0) or ((n % 2 == 0) and (6<= n <= 20)):
+        print("Weird")
+    elif ((n % 2 == 0) and (n > 20)) or ((2<=n<=5) and (n % 2 == 0)):
+        print("Not Weird")
+
+
+
+if __name__ == '__main__':
+    determine_weird(3)
+    determine_weird(24)
+
