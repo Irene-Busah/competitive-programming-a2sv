14c095c9eaa64fcee31dd7e22f4ef93a7a3978f8
Irene-Busah <i.busah@alumni.alueducation.com>
2026-02-04 12:09:28 +0200

    Implement commonChars method to find common characters in a list of words

--- hackerrank/test.py
+++ hackerrank/test.py
@@ -22,3 +22,27 @@ class Solution:
 if __name__ == '__main__':
     matches = [[1,3],[2,3],[3,6],[5,6],[5,7],[4,5],[4,8],[4,9],[10,4],[10,9]]
     print(Solution().findWinners(matches))
+
+
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        results = []
+        counters = []
+        for i in range(len(words)):
+            counter = Counter(words[i])
+            counters.append(counter)
+        
+        for key in counters[0]:
+            min_count = min(counter.get(key, 0) for counter in counters)
+
+            if min_count > 0:
+                results.extend([key] * min_count)
+        print(results)
+        
+
+
+
+if __name__ == '__main__':
+    words = ["bella","label","roller"]
+    Solution().commonChars(words)
