84f47772f5174fa70135c49966ef00d734f44376
Irene-Busah <i.busah@alumni.alueducation.com>
2026-02-16 11:21:28 +0200

    Add solutions for Codeforces problems: A. Game with a Fraction, A. Stones on the Table, A. Word

---
+++ A_Game_with_a_Fraction.py
@@ -0,0 +1,67 @@
+"""
+Alice and Bob have two integers p and q, and they are playing a game with these numbers. 
+The players take turns, with Alice going first. On their turn, a player can do one of two actions:
+
+- decrease p by one (this action is possible if p>0);
+- decrease q by one (this action is possible if q>1).
+
+The game ends when p=0 and q=1.
+
+Bob wins if at any point during the game the fraction p/q is equal to in value the fraction 2/3. 
+Otherwise, Alice wins.
+
+Given the initial values of p and q, determine the winner of the game if both players play optimally.
+
+Input
+=====
+Each test contains multiple test cases. The first line contains the number of test cases t (1≤t≤104). 
+The description of the test cases follows.
+
+Each input case consists of a single line containing two integers p and q (1≤p,q≤1018).
+
+Output
+======
+For each input case, output:
+    - "Alice" if Alice wins;
+    - "Bob" if Bob wins.
+"""
+
+
+
+from shutil import move
+import sys
+
+
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+
+# getting the number of test cases
+numOfTestCases = getInt()
+
+for _ in range(numOfTestCases):
+    p, q = getIntList()
+
+    # If already 2/3 at start
+    if 3 * p == 2 * q:
+        print("Bob")
+        continue
+
+    kmax = min(p // 2, q // 3)
+
+    if p < q and kmax >= (q - p):
+        print("Bob")
+    else:
+        print("Alice")
+
+
+
+
+
+
---
+++ A_Stones_on_the_Table.py
@@ -0,0 +1,45 @@
+"""
+There are n stones on the table in a row, each of them can be red, green or blue. 
+Count the minimum number of stones to take from the table so that any two neighboring 
+stones had different colors. Stones in a row are considered neighboring if there are 
+no other stones between them.
+
+Input
+The first line contains integer n (1≤n≤50) — the number of stones on the table.
+
+The next line contains string s, which represents the colors of the stones. 
+We'll consider the stones in the row numbered from 1 to n from left to right. 
+Then the i-th character s equals "R", if the i-th stone is red, "G", if it's green 
+and "B", if it's blue.
+
+Output
+Print a single integer — the answer to the problem.
+"""
+
+
+import sys
+
+
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+# getting the length of the stone
+numOfStones = getInt()
+
+stones = getStr()
+
+count = 0
+
+for i in range(1, numOfStones):
+    if stones[i] == stones[i-1]:
+        count += 1
+
+
+print(count)
+
