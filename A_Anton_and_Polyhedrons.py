e3ae6e6024dcb751cb1a961058719dc957df5755
Irene-Busah <i.busah@alumni.alueducation.com>
2026-02-21 23:50:43 +0200

    Improve formatting and readability in the problem statement for "A. Anton and Polyhedrons"

--- A_Anton_and_Polyhedrons.py
+++ A_Anton_and_Polyhedrons.py
@@ -1,5 +1,6 @@
 """
-Anton's favourite geometric figures are regular polyhedrons. Note that there are five kinds of regular polyhedrons:
+Anton's favourite geometric figures are regular polyhedrons. Note that there are five kinds of 
+regular polyhedrons:
 
 Tetrahedron. Tetrahedron has 4 triangular faces.
 Cube. Cube has 6 square faces.
@@ -9,12 +10,15 @@ Icosahedron. Icosahedron has 20 triangular faces.
 All five kinds of polyhedrons are shown on the picture below:
 
 
-Anton has a collection of n polyhedrons. One day he decided to know, how many faces his polyhedrons have in total. Help Anton and find this number!
+Anton has a collection of n polyhedrons. One day he decided to know, how many faces his polyhedrons 
+have in total. Help Anton and find this number!
 
 Input
-The first line of the input contains a single integer n (1 ≤ n ≤ 200 000) — the number of polyhedrons in Anton's collection.
+The first line of the input contains a single integer n (1≤n≤200000) — the number of polyhedrons 
+in Anton's collection.
 
-Each of the following n lines of the input contains a string si — the name of the i-th polyhedron in Anton's collection. The string can look like this:
+Each of the following n lines of the input contains a string si — the name of the i-th polyhedron 
+in Anton's collection. The string can look like this:
 
 "Tetrahedron" (without quotes), if the i-th polyhedron in Anton's collection is a tetrahedron.
 "Cube" (without quotes), if the i-th polyhedron in Anton's collection is a cube.
@@ -24,3 +28,17 @@ Each of the following n lines of the input contains a string si — the name of 
 Output
 Output one number — the total number of faces in all the polyhedrons in Anton's collection.
 """
+
+
+import sys
+
+
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
