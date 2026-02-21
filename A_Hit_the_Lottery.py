a78d4179edf42d3033ad77e6ef6ca35fb95397c1
Irene-Busah <i.busah@alumni.alueducation.com>
2026-02-21 23:39:56 +0200

    Add problem statement and solution for "A. Hit the Lottery"

---
+++ A_Hit_the_Lottery.py
@@ -0,0 +1,19 @@
+"""
+Allen has a LOT of money. He has 𝑛
+ dollars in the bank. For security reasons, he wants to withdraw it in cash (we will not disclose the reasons here). The denominations for dollar bills are 1
+, 5
+, 10
+, 20
+, 100
+. What is the minimum number of bills Allen could receive after withdrawing his entire balance?
+
+Input
+The first and only line of input contains a single integer 𝑛
+ (1≤𝑛≤109
+).
+
+Output
+Output the minimum number of bills that Allen could receive.
+
+
+"""
