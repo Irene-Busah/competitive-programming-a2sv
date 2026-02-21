f401d28640f93a1c4096282769ef8ddefb089109
Irene-Busah <i.busah@alumni.alueducation.com>
2026-02-22 01:37:31 +0200

    Implement input functions and logic for calculating the bitwise difference of two binary numbers

--- A_Ultra_Fast_Mathematician.py
+++ A_Ultra_Fast_Mathematician.py
@@ -31,5 +31,32 @@ Write one line — the corresponding answer. Do not omit the leading 0s.
 """
 
 
+import sys
+
+
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+firstNumPair = getStr()
+secondNumPair = getStr()
+
+res = []
+
+for i in range(len(firstNumPair)):
+    if firstNumPair[i] != secondNumPair[i]:
+        res.append(1)
+    else:
+        res.append(0)
+
+numStr = "".join(str(d) for d in res)
+
+print(numStr)
+
 
 
