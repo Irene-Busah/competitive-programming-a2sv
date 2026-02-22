090fcb6914b57f0f8a9ed665347dc4f34313a290
Irene-Busah <i.busah@alumni.alueducation.com>
2026-02-22 02:47:19 +0200

    Implement logic to calculate the minimum number of bills for cash withdrawal

--- A_Hit_the_Lottery.py
+++ A_Hit_the_Lottery.py
@@ -11,5 +11,30 @@ Output the minimum number of bills that Allen could receive.
 """
 
 
+import sys
+
+
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+notes = [100, 20, 10, 5, 1]
+
+
+# getting the balance
+money = getInt()
+
+count = 0
+
+for note in notes:
+    count += money // note
+    money %= note
+
+print(count)
 
 
