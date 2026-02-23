f42cbcd99ba43292966891a1b5136a9c8e60396c
Irene-Busah <i.busah@alumni.alueducation.com>
2026-02-23 17:49:17 +0200

    Add solutions for Codeforces problems: A. Amusing Joke, A. Sum, A. Sum of Round Numbers, and B. Drinks

---
+++ A_Sum.py
@@ -0,0 +1,39 @@
+"""
+You are given three integers a, b, and c. Determine if one of them is the sum of the other two.
+
+Input
+The first line contains a single integer t (1≤t≤9261) — the number of test cases.
+
+The description of each test case consists of three integers a, b, c(0≤a,b,c≤20).
+
+Output
+For each test case, output "YES" if one of the numbers is the sum of the other two, and "NO" otherwise.
+
+You can output the answer in any case (for example, the strings "yEs", "yes", "Yes" and "YES" 
+will be recognized as a positive answer).
+"""
+
+
+
+import sys
+
+
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+# number of test cases
+numOfTestCases = getInt()
+
+for _ in range(numOfTestCases):
+    a, b, c = getIntList()
+
+    if sum([a, b]) == c or sum([a, c]) == b or sum([b, c]) == a:
+        print("YES")
+    else:
+        print("NO")
