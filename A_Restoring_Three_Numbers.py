ac24ca51cd6783ab38ab1188ef4c7f1c6cb33bbc
Irene-Busah <i.busah@alumni.alueducation.com>
2026-02-24 19:34:47 +0200

    completed the solution for the restoring numbers problem

---
+++ A_Restoring_Three_Numbers.py
@@ -0,0 +1,58 @@
+"""
+Polycarp has guessed three positive integers a, b and c. He keeps these numbers in secret, 
+but he writes down four numbers on a board in arbitrary order — their pairwise sums (three numbers) 
+and sum of all three numbers (one number). So, there are four numbers on a board in random order: a+b,
+a+c, b+c and a+b+c
+
+
+You have to guess three numbers a, b and c using given numbers. Print three guessed integers 
+in any order.
+
+Pay attention that some given numbers a, b and c can be equal (it is also possible that a=b=c).
+
+Input
+The only line of the input contains four positive integers x1,x2,x3,x4 (2≤x≤109) — 
+numbers written on a board in random order. It is guaranteed that the answer exists 
+for the given number x1,x2,x3,x4
+
+
+Output
+Print such positive integers a, b and c that four numbers written on a board are values a+b, a+c, b+c
+and a+b+c written in some order. Print a, b and c in any order. If there are several answers, 
+you can print any. It is guaranteed that the answer exists.
+
+
+a+b = 3
+a+c = 6
+b+c = 5
+a+b+c = 4
+"""
+
+
+import sys
+
+
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+
+# getting the data values
+data = getIntList()
+
+data = sorted(data)
+
+aPlusB, aPlusC, bPlusC, aPlusBPlusC = data
+
+a = aPlusBPlusC - bPlusC
+b = aPlusBPlusC - aPlusC
+c = aPlusBPlusC - aPlusB
+
+
+print(a, b, c)
+
