1bae960b4ec9ba0ea5491d621ae434dac48a8a20
Irene-Busah <i.busah@alumni.alueducation.com>
2026-02-25 02:20:44 +0200

    codeforces: completed the solution for the lucky problem

--- A_Lucky.py
+++ A_Lucky.py
@@ -19,6 +19,34 @@ will be recognized as a positive answer).
 """
 
 
+import sys
 
 
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+
+# getting input data
+numOfTestCases = getInt()
+
+
+for _ in range(numOfTestCases):
+    ticket = getStr()
+
+    ticket = [int(x) for x in ticket]
+
+    # print(ticket)
+
+    # print(ticket[3:], ticket[:-3])
+
+    if sum(ticket[3:]) == sum(ticket[:-3]):
+        print("YES")
+    else:
+        print("NO")
 
