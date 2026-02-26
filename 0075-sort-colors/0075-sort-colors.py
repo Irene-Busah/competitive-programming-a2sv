749ee07227cdd35ac28145368f33239e7e086c10
Irene Busah <84809856+Irene-Busah@users.noreply.github.com>
2026-02-26 17:07:24 +0200

    Time: 3 ms (11.3%), Space: 19.2 MB (78.32%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,6 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # First Approach - faster
+        # nums.sort()
 
-        nums.sort()
+
+        # Second approach - O(n*n) - bubble sort
+        for i in range(len(nums)):
+            for j in range(len(nums) - i - 1):
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
         
