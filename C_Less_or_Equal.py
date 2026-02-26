204e6e0a81fdfb740d4a702ab65a5a23fda29312
Irene-Busah <i.busah@alumni.alueducation.com>
2026-02-26 21:55:53 +0200

    codeforces: completed the solution to Again_Twenty_Five problem

---
+++ C_Less_or_Equal.py
@@ -0,0 +1,37 @@
+"""
+You are given a sequence of integers of length n and integer number k. You should print any integer number x
+in the range of [1;109] (i.e. 1≤x≤10^9) such that exactly k elements of given sequence are less than or equal to x
+
+
+Note that the sequence can contain equal elements.
+
+If there is no such x, print "-1" (without quotes).
+
+Input
+=====
+The first line of the input contains integer numbers n and k (1≤n≤2⋅10^5, 0≤k≤n). The second line of the input contains n
+integer numbers a1,a2,…,an (1≤ai≤10^9) — the sequence itself.
+
+Output
+Print any integer number x from range [1;10^9] such that exactly k elements of given sequence is less or equal to x
+
+
+If there is no such x, print "-1" (without quotes).
+"""
+
+
+import sys
+
+
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+
+
+
--- hackerrank/matrix.py
+++ hackerrank/matrix.py
@@ -584,15 +584,67 @@ Output: [22,28,8,6,17,44]
 
 
 
+"""
+Given an array of integers arr, sort the array by performing a series of pancake flips.
 
+In one pancake flip we do the following steps:
+
+Choose an integer k where 1 <= k <= arr.length.
+Reverse the sub-array arr[0...k-1] (0-indexed).
+For example, if arr = [3,2,1,4] and we performed a pancake flip choosing k = 3, we reverse 
+the sub-array [3,2,1], so arr = [1,2,3,4] after the pancake flip at k = 3.
+
+Return an array of the k-values corresponding to a sequence of pancake flips that sort arr. 
+Any valid answer that sorts the array within 10 * arr.length flips will be judged as correct.
+
+ 
+
+Example 1:
+===========
+Input: arr = [3,2,4,1]
+Output: [4,2,4,3]
+Explanation: 
+We perform 4 pancake flips, with k values 4, 2, 4, and 3.
+Starting state: arr = [3, 2, 4, 1]
+After 1st flip (k = 4): arr = [1, 4, 2, 3]
+After 2nd flip (k = 2): arr = [4, 1, 2, 3]
+After 3rd flip (k = 4): arr = [3, 2, 1, 4]
+After 4th flip (k = 3): arr = [1, 2, 3, 4], which is sorted.
+Example 2:
+
+Input: arr = [1,2,3]
+Output: []
+Explanation: The input is already sorted, so there is no need to flip anything.
+Note that other answers, such as [3, 3], would also be accepted.
+"""
+
+
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        res = []
+
+        for n in range(len(arr), 1, -1):
+            i = arr.index(n)
+
+            if i == n - 1:  # already in correct position
+                continue
+
+            if i != 0:
+                res.append(i + 1)
+                arr[:i + 1] = reversed(arr[:i + 1])  # bring n to front
+
+            res.append(n)
+            arr[:n] = reversed(arr[:n])  # move n to position n-1
+
+        return res
 
 
 
 
 if __name__ == '__main__':
-    arr1 = [4, 1, 3, 9, 7]
+    arr = [3,2,4,1]
 
     # names = ["Alice","Bob","Bob"]
     # heights = [155,185,150]
 
-    Solution().selectionSort(arr1)
+    print(Solution().pancakeSort(arr))
