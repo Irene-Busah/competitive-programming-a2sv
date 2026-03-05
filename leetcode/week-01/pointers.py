6d3086e2873ebf77f133ab1a8571f8a42826f70e
Irene-Busah <i.busah@alumni.alueducation.com>
2026-03-05 17:43:07 +0200

    leetcode: submitted another solution

--- leetcode/week-01/pointers.py
+++ leetcode/week-01/pointers.py
@@ -17,6 +17,7 @@ Output: [0]
 """
 
 
+from itertools import count
 from typing import Counter, List
 
 
@@ -175,24 +176,87 @@ The third distinct maximum is 1.
 """
        
 
+# class Solution:
+#     def thirdMax(self, nums: List[int]) -> int:
+
+#         newArray = list(set(nums))
+
+#         newArray.sort(reverse=True)
+
+#         if len(nums) >= 3:
+#             print(newArray[2], newArray)
+#         else:
+#             print(max(newArray), newArray)
+#         # print(nums)
+
+
+
+
+
+"""
+You are given a 0-indexed integer array players, where players[i] represents the ability of 
+the ith player. You are also given a 0-indexed integer array trainers, where trainers[j] 
+represents the training capacity of the jth trainer.
+
+The ith player can match with the jth trainer if the player's ability is less than or 
+equal to the trainer's training capacity. Additionally, the ith player can be matched 
+with at most one trainer, and the jth trainer can be matched with at most one player.
+
+Return the maximum number of matchings between players and trainers that satisfy these conditions.
+
+ 
+
+Example 1:
+==========
+Input: players = [4,7,9], trainers = [8,2,5,8]
+Output: 2
+Explanation:
+One of the ways we can form two matchings is as follows:
+- players[0] can be matched with trainers[0] since 4 <= 8.
+- players[1] can be matched with trainers[3] since 7 <= 8.
+It can be proven that 2 is the maximum number of matchings that can be formed.
+
+
+Example 2:
+==========
+Input: players = [1,1,1], trainers = [10]
+Output: 1
+Explanation:
+The trainer can be matched with any of the 3 players.
+Each player can only be matched with one trainer, so the maximum answer is 1.
+
+"""
+
+
 class Solution:
-    def thirdMax(self, nums: List[int]) -> int:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        # sorting the array
+        players.sort()
+        trainers.sort()
 
-        newArray = list(set(nums))
+        first = 0
+        second = 0
 
-        newArray.sort(reverse=True)
+        count = 0
 
-        if len(nums) >= 3:
-            print(newArray[2], newArray)
-        else:
-            print(max(newArray), newArray)
-        # print(nums)
+        
+        while first < len(players) and second < len(trainers):
+            if trainers[second] >= players[first]:
+                count += 1
 
+                first += 1
+                second += 1
+            else:
+                second += 1
+
+        print(count)
 
 
 if __name__ == '__main__':
-    nums = [3,2,1]
-    # nums = [1,2]
+    players = [4,7,9]
+    trainers = [8,2,5,8]
 
-    # nums = [2,2,3,1]
-    Solution().thirdMax(nums)
+    players = [1,1,1]
+    trainers = [10]
+    Solution().matchPlayersAndTrainers(players, trainers)
+
