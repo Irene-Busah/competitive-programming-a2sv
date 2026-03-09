54096515d3b9d69b66721040291a87d721fbbb05
Irene-Busah <i.busah@alumni.alueducation.com>
2026-03-09 15:57:01 +0200

    leetcode: solution to the Palindrome string problem

--- leetcode/week-01/sort.py
+++ leetcode/week-01/sort.py
@@ -26,6 +26,7 @@ You have 3 cookies and their sizes are big enough to gratify all of the children
 You need to output 2.
 """
 
+from curses.ascii import isalnum
 from itertools import count
 from typing import List
 
@@ -107,17 +108,72 @@ Output: 10
 """
 
 
-class Solution:
-    def countPairs(self, nums: List[int], target: int) -> int:
+# class Solution:
+#     def countPairs(self, nums: List[int], target: int) -> int:
 
+#         i, j = 0, len(nums)-1
+
+#         nums.sort()
+
+#         count = 0
+
+#         while i < j:
+#             if nums[i] + nums[j] < target:
+#                 count += j - i
+#                 i += 1
+#             else:
+#                 j -= 1
+#         print(count)
+
+
+
+"""
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all 
+non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+"""
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i, j = 0, len(s)-1
+
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i].lower() != s[j].lower():
+                return False
+            i+=1
+            j-=1        
+
+        return True
 
 
 
 if __name__  == '__main__':
-    g = [1,2,3]
-    s = [1,1]
+    s = "A man, a plan, a canal: Panama"
 
-    g = [1,2]
-    s = [1,2,3]
-
-    Solution().findContentChildren(g, s)
+    Solution().isPalindrome(s)
