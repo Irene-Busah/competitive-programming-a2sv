8ed76a1c18736fae6bde76abb61d736aaf9f851a
Irene-Busah <i.busah@alumni.alueducation.com>
2026-03-09 15:02:55 +0200

    codeforces: completed another solution - A_Vanya_and_Cubes

--- A_Vanya_and_Cubes.py
+++ A_Vanya_and_Cubes.py
@@ -15,3 +15,34 @@ Print the maximum possible height of the pyramid in the single line.
 
 
 
+# importing necessary libraries
+import sys
+
+
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
+
+
+# reading the dat
+cubes = getInt()
+
+height = 0
+level = 1
+
+levelCubes = 0
+
+while cubes >= level + levelCubes:
+    levelCubes += level
+
+    cubes -= levelCubes
+
+    height += 1
+    level += 1
+    
+
+print(height)
--- leetcode/week-01/sort.py
+++ leetcode/week-01/sort.py
@@ -77,12 +77,40 @@ Output: []
 """
 
 
-class Solution:
-    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+# class Solution:
+#     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         
-        pass
+#         pass
 
-        
+
+
+
+
+"""
+Given a 0-indexed integer array nums of length n and an integer target, return the number of 
+pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+ 
+
+Example 1:
+
+Input: nums = [-1,1,2,3,1], target = 2
+Output: 3
+Explanation: There are 3 pairs of indices that satisfy the conditions in the statement:
+- (0, 1) since 0 < 1 and nums[0] + nums[1] = 0 < target
+- (0, 2) since 0 < 2 and nums[0] + nums[2] = 1 < target 
+- (0, 4) since 0 < 4 and nums[0] + nums[4] = 0 < target
+Note that (0, 3) is not counted since nums[0] + nums[3] is not strictly less than the target.
+Example 2:
+
+Input: nums = [-6,2,5,-2,-7,-1,3], target = -2
+Output: 10
+"""
+
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+
+
 
 
 if __name__  == '__main__':
