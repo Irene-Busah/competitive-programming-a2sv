b2af7da4cc5602e277fc6402914b03d55d553c91
Irene-Busah <i.busah@alumni.alueducation.com>
2026-03-10 19:53:01 +0200

    codeforces: completed the solution to the A_Vlad_and_the_Best_of_Five problem

--- A_Vlad_and_the_Best_of_Five.py
+++ A_Vlad_and_the_Best_of_Five.py
@@ -18,7 +18,40 @@ frequently in the string.
 
 
 
+# importing necessary libraries
+import sys
+
+
+# defining useful function for input data collection
+def getInt(): return int(sys.stdin.readline().strip())
+def getStr(): return sys.stdin.readline().strip()
+def getIntSeq(): return map(int, sys.stdin.readline().strip().split())
+def getStrSeq(): return sys.stdin.readline().strip().split()
+def getIntList(): return list(map(int, sys.stdin.readline().strip().split()))
+def getStrList(): return list(sys.stdin.readline().strip().split())
 
 
 
+# getting input data
+numOfTestCases = getInt()
 
+for _ in range(numOfTestCases):
+    string = getStr()
+
+    counter = {}
+
+    for i in range(len(string)):
+        if string[i] not in counter:
+            counter[string[i]] = 1
+        else:
+            counter[string[i]] += 1
+    
+
+    maxVal = 0
+    ans = ''
+    for key, val in counter.items():
+        if val > maxVal:
+            maxVal = val
+            ans = key
+
+    print(ans)
