6ab6b2835f4f13f11847fbd253c83c2e840fea77
Irene-Busah <i.busah@alumni.alueducation.com>
2026-03-18 00:30:10 +0200

    update: re-arranged files

--- leetcode/week-02/pointers.py
+++ leetcode/week-02/pointers.py
@@ -34,20 +34,34 @@ There is no way to divide the players into teams such that the total skill of ea
 """
 
 
-from typing import List
+from typing import Counter, List
 
 
-class Solution:
-    def dividePlayers(self, skill: List[int]) -> int:
-        skill.sort()
+# class Solution:
+#     def dividePlayers(self, skill: List[int]) -> int:
+#         skill.sort()
 
-        teamTotal = skill[0] + skill[-1]
-        chemistrySum = 0
+#         skillTotal = sum(skill)
+#         teams = len(skill) // 2
 
-        for i in range(len(skill) / 2):
-            pass
+#         if skillTotal % teams != 0:
+#             return -1
 
+#         res = 0
+#         teamSum = skillTotal // teams
+            
+        
+#         left, right = 0, len(skill)-1
 
+#         while left < right:
+#             if skill[left] + skill[right] != teamSum:
+#                 return -1
+#             res += skill[left] * skill[right]
+
+#             left += 1
+#             right -= 1
+
+#         print(res)
 
 
 """
@@ -125,7 +139,147 @@ Explanation: The vowel substrings of word are as follows (underlined):
 #         print(count)
 
 
+
+
+"""
+Given two strings s and p, return an array of all the start indices of p's anagrams in s. 
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "cbaebabacd", p = "abc"
+Output: [0,6]
+Explanation:
+The substring with start index = 0 is "cba", which is an anagram of "abc".
+The substring with start index = 6 is "bac", which is an anagram of "abc".
+Example 2:
+
+Input: s = "abab", p = "ab"
+Output: [0,1,2]
+Explanation:
+The substring with start index = 0 is "ab", which is an anagram of "ab".
+The substring with start index = 1 is "ba", which is an anagram of "ab".
+The substring with start index = 2 is "ab", which is an anagram of "ab".
+"""
+
+
+# class Solution:
+#     def findAnagrams(self, s: str, p: str) -> List[int]:
+
+#         res = []
+
+#         if len(p) > len(s):
+#             print(res)
+
+#         counter_p = Counter(p)
+#         window = Counter()
+
+#         lenOfP = len(p)
+
+#         for i in range(len(s)):
+#             window[s[i]] += 1
+
+#             if i >= lenOfP:
+#                 if window[s[i-lenOfP]] == 1:
+#                     del window[s[i-lenOfP]]
+#                 else:
+#                     window[s[i-lenOfP]] -= 1
+            
+#             if window == counter_p:
+#                 res.append(i-lenOfP+1)
+            
+#         print(res)
+
+
+
+"""
+Given an array of integers nums and an integer k. A continuous subarray is called nice if there are k odd numbers on it.
+
+Return the number of nice sub-arrays.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2,1,1], k = 3
+Output: 2
+Explanation: The only sub-arrays with 3 odd numbers are [1,1,2,1] and [1,2,1,1].
+Example 2:
+
+Input: nums = [2,4,6], k = 1
+Output: 0
+Explanation: There are no odd numbers in the array.
+Example 3:
+
+Input: nums = [2,2,2,1,2,2,1,2,2,2], k = 2
+Output: 16
+"""
+
+# class Solution:
+#     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+#         ans = 0
+
+#         window = []
+
+#         for i in range(len(nums)):
+#             if nums[i] % 2 != 0:
+#                 window.append(1)
+#             else:
+#                 window.append(0)
+
+        
+#         # left, right = 0, left + 1
+        
+#         for start in range(len(window)):
+#             curr_sum = 0
+
+#             for end in range(start, len(window)):
+#                 curr_sum += window[end]
+
+#                 if curr_sum == k:
+#                     ans += 1
+
+#                 if curr_sum > k:
+#                     break
+
+#         return ans
+
+
+
+# class Solution:
+#     def runningSum(self, nums: List[int]) -> List[int]:
+#         res = []
+
+#         currSum = 0
+
+#         for i in range(len(nums)):
+#             currSum += nums[i]
+#             res.append(currSum)
+        
+#         print(res)
+
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def sumRange(self, left: int, right: int) -> int:
+        res = []
+
+        currSum = 0
+        for i in range(left, right+1):
+            res.append(currSum)
+
+            currSum += self.nums[i]
+
+        
+        return currSum
+
+
 if __name__ == '__main__':
-    word = "aeiouu"
-    Solution().countVowelSubstrings(word)
+    ["NumArray", "sumRange", "sumRange", "sumRange"]
+    [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
 
