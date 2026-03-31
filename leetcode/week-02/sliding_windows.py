8e6acba748ad5aaf242378dd17919edd212d3e9e
Irene-Busah <i.busah@alumni.alueducation.com>
2026-03-31 15:42:54 +0200

    update: re-arranged some files

--- leetcode/week-02/sliding_windows.py
+++ leetcode/week-02/sliding_windows.py
@@ -120,42 +120,65 @@ Print one integer, the length of the longest good segment. If there are no such 
 
 
 """
-Given an array nums of n integers, return an array of all the unique quadruplets 
-[nums[a], nums[b], nums[c], nums[d]] such that:
+You are given an array of positive integers arr. Perform some operations (possibly none) 
+on arr so that it satisfies these conditions:
 
-0 <= a, b, c, d < n
-a, b, c, and d are distinct.
-nums[a] + nums[b] + nums[c] + nums[d] == target
-You may return the answer in any order.
+The value of the first element in arr must be 1.
+The absolute difference between any 2 adjacent elements must be less than or equal to 1. 
+In other words, abs(arr[i] - arr[i - 1]) <= 1 for each i where 1 <= i < arr.length (0-indexed). 
+abs(x) is the absolute value of x.
+There are 2 types of operations that you can perform any number of times:
+
+Decrease the value of any element of arr to a smaller positive integer.
+Rearrange the elements of arr to be in any order.
+Return the maximum possible value of an element in arr after performing the operations to satisfy 
+the conditions.
+
+ 
 
 Example 1:
 
-Input: nums = [1,0,-1,0,-2,2], target = 0
-Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Input: arr = [2,2,1,2,1]
+Output: 2
+Explanation: 
+We can satisfy the conditions by rearranging arr so it becomes [1,2,2,2,1].
+The largest element in arr is 2.
 Example 2:
 
-Input: nums = [2,2,2,2,2], target = 8
-Output: [[2,2,2,2]]
+Input: arr = [100,1,1000]
+Output: 3
+Explanation: 
+One possible way to satisfy the conditions is by doing the following:
+1. Rearrange arr so it becomes [1,100,1000].
+2. Decrease the value of the second element to 2.
+3. Decrease the value of the third element to 3.
+Now arr = [1,2,3], which satisfies the conditions.
+The largest element in arr is 3.
+
 """
 
-
-
 class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        ans = []
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
 
-        left, right = 0, 0
+        currMax = 0
 
-        while right < len(nums):
-            pass
+        for a in arr:
+            if a > currMax + 1:
+                a = currMax + 1
+            
+            currMax = a
+        
+        print(currMax)
 
 
 
 
+if __name__ == '__main__':
+    arr = [2,2,1,2,1]
 
-# if __name__ == '__main__':
-#     s = "abcabcbb"
+    arr = [100,1,1000]
 
-#     print(Solution().lengthOfLongestSubstring(s))
+    Solution().maximumElementAfterDecrementingAndRearranging(arr)
 
 
